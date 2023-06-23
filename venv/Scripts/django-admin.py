c56ba529769e349651714f74a7f843813dc83e1e
marychevnikolay <nikolaymarycheff@gmail.com>
2023-06-23 19:47:03 +0300

    first_commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Николай\OneDrive\Документы\project\django-db\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
